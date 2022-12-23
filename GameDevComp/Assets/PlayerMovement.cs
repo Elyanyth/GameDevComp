d07a947bbd2b090de717cacd1bbb04e209f60d45
Elyanyth <58537719+Elyanyth@users.noreply.github.com>
2022-12-23 17:01:25 -0400

    Fixed plrMovement
    Fixed sliding with player mouvement

---
+++ GameDevComp/Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MovementSpeed = 5f; //Mouvement Speed
+    public Rigidbody2D rb; 
+    Vector2 movement;
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate() 
+    {
+        rb.MovePosition(rb.position + movement * MovementSpeed * Time.fixedDeltaTime);
+    }
+
+}
--- GameDevComp/Assets/Script.cs
+++ GameDevComp/Assets/Script.cs
@@ -2,26 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movement : MonoBehaviour
+public class Script : MonoBehaviour
 {
     Rigidbody2D rb;
-    public int speed = 10;
-    bool grounded;
-
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float inputX = Input.GetAxis("Horizontal");
-        float inputY = Input.GetAxis("Vertical");
-
-        Vector2 movement = new Vector2(speed * inputX, speed * inputY);
-
-        rb.AddForce(movement);
+        
+        
     }
 }
